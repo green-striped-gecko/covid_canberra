d1bccfed9cd55496db9ad98813037064abd9dfc1
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2021-08-16 21:19:25 +1000

    update next

--- gecode_tables.R
+++ gecode_tables.R
@@ -2,13 +2,22 @@ library(leaflet)
 library(ggmap)
 library(rvest)
 
+fixgeo <- function(search,tab,  lat, lon, tt=tabs) {
+  
+  ii <- NA
+  ii <- grep(search,tt[[tab]]$Place)
+  for (c in 1:length(ii)){
+    tt[[tab]][ii[c],"lat"] <-lat
+    tt[[tab]][ii[c],"lon"] <- lon
+  }
+  return(tt)
+}
+
 #load google api
 gapi <- readLines("gapi.txt")
 register_google(gapi)
 
 #grab from website
-
-
 es <- read_html("https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations")
 
 #check update
@@ -22,9 +31,6 @@ lu <- gsub(" ", "_",lu)
 lu <- gsub(":","",lu)
 
 
-
-
-
 #grab tables
 tnames <- c("Close_Contacts", "Casual_Contacts","Monitor_for_symptoms")
 es <- read_html("https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations")
@@ -36,8 +42,22 @@ tbls_ls <- es %>%
   html_table(fill = TRUE)
 #there are three tables
 
+#check if there was an update....
 
-m <- leaflet() %>% addTiles()
+ff <- list.files("./data/")
+
+wu <- grep(lu, ff)
+
+if(length(wu)>0) stop("No updated since. Current data is from:", lu,"\n") 
+
+
+if (length(wu)==0)
+{
+unlist(lapply(tbls_ls, nrow))
+
+
+
+
 tabs <- list()
 cols <- c("red", "yellow", "blue")
 for (i in 1:3)
@@ -51,31 +71,37 @@ address <- geocode(paste(tab$Place,", Canberra, Australia"))
 
 tab$lat <- address$lat
 tab$lon <- address$lon
-
-write.csv(tab, paste0("./data/",tnames[i],"_",lu,".csv"),row.names = FALSE )
-
-
-# Aggregate method
-labs <- paste(tab$Place, tab$Date,tab$Arrival.Time, tab$Departure.Time, sep="<br/>") 
-
-
-
-m <- m %>% addCircleMarkers(lat=tab$lat, lng=tab$lon,popup = labs, weight=0.5, color = cols[i], radius = 3 , fillOpacity = 0.8)
-
 tabs[[i]] <- tab
 }
-
-m
 names(tabs)<- tnames
+##errors (manual)
+##oakley in casual 2x  [-35.24167	149.058]
+tabs <- fixgeo("TLE Electrical & Data supplies, 22-36 Oatley Court",tab = 2, lat=-35.24167, lon = 149.058)
+
+##aranda in monitoring [-35.2536 ) 
+tabs <- fixgeo("Aranda Playing Fields", tab=3, lat=-35.25363, lon=149.08)
+
+
+m <- leaflet() %>% addTiles()
+for (i in 1:3) {
+# Aggregate method
+labs <- paste(tabs[[i]]$Place, tabs[[i]]$Date,tabs[[i]]$Arrival.Time, tabs[[i]]$Departure.Time, sep="<br/>") 
+
+m <- m %>% addCircleMarkers(lat=tabs[[i]]$lat, lng=tabs[[i]]$lon,popup = labs, weight=0.5, color = cols[i], radius = 5 , fillOpacity = 0.8)
+
+}
+
+#checks 
+m
+
 
 lapply(tabs,function(x) summary(x$lat) )
 lapply(tabs,function(x) summary(x$lon) )
 
 
 
-#errors (manual)
-
-#oakley in casual 2x  [-35.24167	149.058]
-#aranda in monitoring [-35.2536	149.08]
-
-
+for (i in 1:3)
+{
+write.csv(tabs[[i]], paste0("./data/",tnames[i],"_",lu,".csv"),row.names = FALSE )
+}
+}  #wu >0 = new data is there....
