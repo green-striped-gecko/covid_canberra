54fffa747667e17dcb2f53e384ca18450ae893b9
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2021-08-18 15:57:09 +1000

    automatic email with Task Scheduler

--- check_update_email.r
+++ check_update_email.r
@@ -1,7 +1,7 @@
 #monitor updates
 library(rvest)
-library(sendmailR)
-last <- readLines("lastupdated.csv")
+library(DescTools)
+last <- readLines("c:/Bernd/R/covid_canberra/lastupdated.csv")
 
 #grab from website
 es <- read_html("https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations")
@@ -13,6 +13,7 @@ ll <- es %>%
 index <- grep("Page last updated:",ll)
 current <- ll[index]
 
+
 if (last!=current)
 {
  
@@ -23,5 +24,6 @@ if (last!=current)
   SendOutlookMail(to = c("luis.mijangosaraujo@canberra.edu.au"), 
                   subject = "The has been a covid update.", 
                   body = paste0("New update is:\n", current))
-}
+
+  }
 
