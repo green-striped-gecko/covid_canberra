31205bac4554a9211bae4a6883d2648d2564e910
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2021-08-20 13:16:18 +1000

    test automatic email updates

--- check_update_email.r
+++ check_update_email.r
@@ -1,3 +1,5 @@
+
+stop("stop for now")
 #monitor updates
 library(rvest)
 library(DescTools)
@@ -21,8 +23,17 @@ if (last!=current)
   #scomp <- NULL
  
   source("c:/Bernd/R/covid_canberra/gecode_tables.R")
-
-  body <- paste0("New update is: ", current,"\n")
+  
+  
+######################################################################
+  
+  body <- paste0("New update is from: ", current,"\n Please be aware data have not been curated yet and locations are assigned via a computer script.\n Therefore locations might be in the wrong place. \nPlease report locations that need to be corrected to: maybe a wiki page???\n Covid resources: 
+                 \nACT health pages (official): https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations
+                 \nACT health map: https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations/map
+                 \nThis map: https://green-striped-gecko.github.io/covid_canberra/
+                 \nCovid near me map: https://covid19nearme.com.au/state/act
+                 
+                 ")
   attach <- kable(list(scomp$comparison.summary.table, scomp$diffs.byvar.table))
    dlat <- paste0("range of lats:",paste0(range(ldata$lat), collapse = " to "))
    dlon <- paste0("range of lons:",paste0(range(ldata$lon), collapse = " to "))
@@ -30,12 +41,14 @@ if (last!=current)
   writeLines(attach,"c:/Bernd/R/covid_canberra/comparison/attach.txt")
   
   mapshot(nm, file = "c:/Bernd/R/covid_canberra/comparison/newsites.png")
- 
-  SendOutlookMail(to = c("bernd.gruber@canberra.edu.au"), 
-                  subject = paste0("There has been a covid update. ", lup), 
+  tolist <-  c("bernd.gruber@canberra.edu.au")
+  #tolist <- c("bernd.gruber@canberra.edu.au", "Luis.MijangosAraujo@canberra.edu.au", "Anthony.Davidson@canberra.edu.au")
+  
+  SendOutlookMail(to = paste(tolist,sep="", collapse="; "), 
+                  subject = paste0("New Covid Exposure sites have been added.\n ", lup), 
                   body = body, attachment = c("c:/bernd/r/covid_canberra/comparison/attach.txt", "c:/bernd/r/covid_canberra/comparison/newsites.png"))
   
-
+############################################################################
   writeLines(current, "c:/Bernd/R/covid_canberra/lastupdated.csv")
 
   }
