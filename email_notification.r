efc8cb493a258ece4c61bd5d50cf6bcc12d850b1
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2021-08-21 21:32:55 +1000

    send via bcc instead of to

--- email_notification.r
+++ email_notification.r
@@ -63,12 +63,12 @@ writeLines(attach,"c:/Bernd/R/covid_canberra/comparison/changes_since_last_updat
 
 mapshot(nm, file = "c:/Bernd/R/covid_canberra/comparison/newsites_email.png")
 tolist <-  c("bernd.gruber@canberra.edu.au")
-#tolist <- c("bernd.gruber@canberra.edu.au", "Luis.MijangosAraujo@canberra.edu.au", "Anthony.Davidson@canberra.edu.au")
+tolist <- c("bernd.gruber@canberra.edu.au", "Luis.MijangosAraujo@canberra.edu.au", "Anthony.Davidson@canberra.edu.au","Llara.Weaver <Llara.Weaver@canberra.edu.au>")
 
 #tolist <- c("Bernd Gruber <bernd.gruber@canberra.edu.au>","Llara.Weaver <Llara.Weaver@canberra.edu.au>")
 
 
-SendOutlookMail(to = paste(tolist,sep="", collapse="; "), 
+SendOutlookMail(to="bernd.gruber@canberra.edu.au",bcc = paste(tolist,sep="", collapse="; "), 
                 subject = paste0("New Covid Exposure sites have been added.\n ", lup), 
                 body = body, attachment = c("c:/bernd/r/covid_canberra/comparison/changes_since_last_update.txt", "c:/bernd/r/covid_canberra/comparison/newsites_email.png"))
 
