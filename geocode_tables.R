71f8b8e9908bfd2dc57f0c57a2b71ae3168a12ef
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2021-08-22 14:51:42 +1000

    added bus routes

--- geocode_tables.R
+++ geocode_tables.R
@@ -7,8 +7,10 @@ library(DescTools)
 library(knitr)
 library(mapview)
 library(arsenal)
+require(rgdal)
 
 
+addBuses = TRUE
 
 fixgeo <- function(search,  lat, lon, column="Exposure.Location",tt=tab3) {
   
@@ -163,16 +165,13 @@ for ( i in 1:length(index))
 cols <- c( "yellow", "red","cyan", "blue")
 
 labs <- paste(tab3$Contact, tab3$Status,tab3$Exposure.Location, tab3$Street, tab3$Suburb, tab3$Date,tab3$Arrival.Time, tab3$Departure.Time, tab3$doubles, sep="<br/>") 
-
-#check added locations (last update)and make them obvious
-
-
-
-
-
-
-
 cc <- as.numeric(factor(tab3$Contact))
+
+
+
+
+
+
 
 ###############################################
 ##plot the map
@@ -181,7 +180,37 @@ m <- leaflet() %>% addTiles()
 
 m <- m %>% addCircleMarkers(lat=tab3$lat, lng=tab3$lon,popup = labs, weight=0.5, color = cols[cc], radius = 5 , fillOpacity = 0.8)
 m <- m %>% addLegend("bottomright", labels = levels(factor(tab3$Contact)), colors = cols, opacity = 1)
-m 
+
+
+####################################################
+### add bus lines mentioned
+
+
+if (addBuses) {
+#read from shape file
+busses <- readOGR(dsn = "c:/bernd/r/covid_canberra/bus", layer = "geo_export_69c76e06-1d3f-4619-be3b-b4e5789be8ca")
+
+#search all bus lines that are mentioned
+
+bindex <- grep("Bus Route", tab3$Exposure.Location)
+buslanes <- tab3$Exposure.Location[bindex]
+
+busnumbers <- gsub("Bus Route ([0-9,A-Z]+) Transport.*","\\1", buslanes)
+blineindex <- which(busses$short_name %in% busnumbers)
+blabs <- paste(paste0("Bus route: ", busses$short_name[blineindex]),"<strong> For bus stops and ","exposure times, please"," search the table." , sep="<br/>")
+bb <- (busses[blineindex,])
+coo <- coordinates(bb)
+bcols <- colorRampPalette(c("purple", "green"))( length(coo))
+
+for (ib in 1:length(coo))
+{
+  cood <- data.frame(coo[[ib]])
+  m <- m %>% addPolylines(lng=cood[,1], lat=cood[,2], color = bcols[ib], weight   = 3, opacity = 0.4, popup  = blabs[ib])
+}
+
+}
+m
+
 ###############################################
 
  range(tab3$lat) 
